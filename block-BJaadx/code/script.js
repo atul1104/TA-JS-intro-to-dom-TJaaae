9d19e78863df2d9cc554343ea5c5dbe9d0a1af74
atul1104 <waghamareatul2@gmail.com>
2021-12-09 19:23:08 +0530

    assignment

--- block-BJaadx/code/script.js
+++ block-BJaadx/code/script.js
@@ -1,9 +1,10 @@
 // 1. Change the title of the page to `Hello AltCampus!`
-
+document.title = "Hello Altcampus";
 // 2. Select the element using the children property:
 
 //    - Select the `h1` element and change the value to `Learning DOM`
-
+let heading = document.querySelector("h1");
+let heading = "Learning DOM";
 //    - Select the first `li` element inside the `ul` with class `topics` and change the innerText to `all about document`
 //    - Select the input element with name `email`
 
